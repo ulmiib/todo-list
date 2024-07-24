4d514bf2e2228f0c840be28bf345e4ae00bfd1a2
ulmiib <chinguunbat569@gmail.com>
2024-07-24 12:58:04 +0800

    update

--- src/App.js
+++ src/App.js
@@ -28,18 +28,17 @@ function App() {
 
   return (
     <div className="min-h-screen bg-black flex justify-center items-center">
-      <div className="bg-teal-950 p-6 rounded-lg shadow-lg w-full max-w-lg">
-        <h1 className="text-2xl font-bold mb-6">Todo List</h1>
+      <div className="bg-[#102C57] p-6 rounded-lg shadow-lg w-full max-w-xl p-10">
         <div className="flex mb-4">
           <input
             placeholder="Add a new task"
             type="text"
-            className="border border-teal-300 rounded-xl p-2 mr-2 flex-1"
+            className="border border-teal-300 rounded-xl p-2 mr-2 flex-1 max-w-96 min-w-96 min-h-10 bg-transparent"
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
           <button
-            className="bg-blue-500 hover:bg-blue-600 text-white p-2 border rounded-xl"
+            className="bg-[#1679AB] hover:bg-blue-600 text-white p-2 border rounded-xl min-w-10 max-w-10 min-h-10 max-h-10"
             onClick={addTodo}
           >
             <FaPlus />
@@ -50,7 +49,7 @@ function App() {
           {todos.map((todo, index) => (
             <div
               key={index}
-              className="flex items-center justify-between bg-black text-white m-1 p-3 text-xs border rounded-xl"
+              className="flex items-center justify-between bg-black text-white m-3 p-3 text-xs border rounded-xl max-w-md min-h-20"
             >
               {todo}
               <button>
@@ -65,6 +64,7 @@ function App() {
             </div>
           ))}
         </div>
+        {/* <h2 className="text-white text-x">{heading}</h2> */}
       </div>
     </div>
   );
